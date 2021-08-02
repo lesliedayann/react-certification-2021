2c8d3379ad495795f1279c97cc4aa49a8a1e25c8
Leslie Dayanna Figueroa Moreno <leslie.figueroa@ioconnectservices.com>
2021-08-02 18:14:54 -0500

    [NEW] Video search functionality, using YouTube API, fixing eslint issue

--- src/components/Switch/ColoredSwitch.component.jsx
+++ src/components/Switch/ColoredSwitch.component.jsx
@@ -1,38 +1,44 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
-const useStyles = makeStyles((theme) => ({
-  label:{
+
+const useStyles = makeStyles(() => ({
+  label: {
     whiteSpace: 'nowrap',
     fontSize: '1rem',
-  }
+  },
 }));
-const ColoredSwitch = (props)=>{
+const ColoredSwitch = (props) => {
   const classes = useStyles();
 
-  const {switchColor, label, checkedColor ,toggleSwitch, checked}=props;
+  const { switchColor, label, checkedColor, toggleSwitch, checked } = props;
 
-   const CustomSwitch = withStyles({
-        switchBase: {
-          color: switchColor,
-          '&$checked': {
-            color: checkedColor? checkedColor : switchColor,
-          },
-          '&$checked + $track': {
-            backgroundColor: checkedColor? checkedColor : switchColor,
-          },
-        },
-        checked: {},
-        track: {},
-    })(Switch);
+  const CustomSwitch = withStyles({
+    switchBase: {
+      color: switchColor,
+      '&$checked': {
+        color: checkedColor || switchColor,
+      },
+      '&$checked + $track': {
+        backgroundColor: checkedColor || switchColor,
+      },
+    },
+    checked: {},
+    track: {},
+  })(Switch);
 
-  return(
-    <FormGroup >
+  return (
+    <FormGroup>
       <FormControlLabel
         control={
-          <CustomSwitch checked={checked} onChange={toggleSwitch} name='customSwitch' />
+          <CustomSwitch
+            checked={checked}
+            onChange={toggleSwitch}
+            name="customSwitch"
+            id="customSwitch"
+          />
         }
         label={label}
         classes={{
@@ -40,7 +46,7 @@ const ColoredSwitch = (props)=>{
         }}
       />
     </FormGroup>
-  )
+  );
 };
 
 export default ColoredSwitch;
--- src/components/VideosContainer/index.js
+++ src/components/VideosContainer/index.js
@@ -1,1 +1,1 @@
-export {default} from './VideosContainer.component'; 
+export { default } from './VideosContainer.component';
