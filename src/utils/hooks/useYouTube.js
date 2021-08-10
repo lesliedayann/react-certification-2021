eab6df7c87148a7820973a2d8cd3e2ad72cfaae4
Leslie Dayanna Figueroa Moreno <leslie.figueroa@ioconnectservices.com>
2021-08-09 23:29:54 -0500

    [FIX] changing useYouTube from vanilla JS to hooks

--- src/utils/hooks/useYouTube.js
+++ src/utils/hooks/useYouTube.js
@@ -1,44 +1,69 @@
+import { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import { AppContext } from '../AppContext';
 
-const API_KEY = 'AIzaSyDE9yryXzFe5HsYFd1uH3FMIOrp6ldrhZM';
+const API_KEY = 'AIzaSyBjZD4mXWevXhflwEgjpDOIqhF5WMFg_3A';
 
-export const fetchVideos = (searchValue) => {
-  const APIUrl = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&type=video&part=snippet&maxResults=40&q=${searchValue}`;
-  const request = axios.get(APIUrl);
-  return request
-    .then((response) => {
-      if (response.status === 200) {
-        return response.data;
-      }
-      if (response.status === 404) {
-        throw new Error('Not found');
-      }
-      if (!response.ok) {
-        throw new Error('Sorry, something went wrong');
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+export const useFetchVideos = () => {
+  const { search } = useContext(AppContext);
+  const [videos, setVideos] = useState();
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const APIUrl = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&type=video&part=snippet&maxResults=40&q=${search}`;
+      await axios
+        .get(APIUrl)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.data;
+          }
+          if (response.status === 404) {
+            throw new Error('Not found');
+          }
+          if (!response.ok) {
+            throw new Error('Sorry, something went wrong');
+          }
+        })
+        .then((response) => {
+          setVideos(response.items);
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
+    };
+
+    fetchVideos();
+  }, [search]);
+
+  return { videos };
 };
 
-export const fetchVideoById = (id) => {
-  const API_URL = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${id}&key=${API_KEY}`;
-  const request = axios.get(API_URL);
-  return request
-    .then((response) => {
-      if (response.status === 200) {
-        console.log(response);
-        return response.data;
-      }
-      if (response.status === 404) {
-        throw new Error('Not found');
-      }
-      if (!response.ok) {
-        throw new Error('Sorry, something went wrong');
-      }
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+export const useFetchVideoById = (videoId) => {
+  const [videoDetails, setVideoDetails] = useState();
+  useEffect(() => {
+    const fetchVideoById = async () => {
+      const API_URL = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
+      await axios
+        .get(API_URL)
+        .then((response) => {
+          if (response.status === 200) {
+            return response.data;
+          }
+          if (response.status === 404) {
+            throw new Error('Not found');
+          }
+          if (!response.ok) {
+            throw new Error('Sorry, something went wrong');
+          }
+        })
+        .then((response) => {
+          setVideoDetails(response.items[0]);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchVideoById();
+  }, [videoId]);
+  return { videoDetails };
 };
